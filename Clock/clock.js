c7d43b88ab09bed37da55ccac98a133979993161
siimtishler <92330937+siimtishler@users.noreply.github.com>
2022-09-02 14:02:39 +0300

    Update clock.js

--- Clock/clock.js
+++ Clock/clock.js
@@ -2,33 +2,26 @@ const Time = document.getElementById('time-display');
 const SecondsHand = document.getElementById('seconds');
 const MinutesHand = document.getElementById('minutes');
 const HourHand = document.getElementById('hours');
+const cities = document.getElementById('cities');
 
-class TimeData{
-    constructor(cityName, cityOffset){
-        this.cityName = cityName;
-        this.offset = cityOffset;
-        this.time = function(){
-            let date = new Date();
-            let localTime = date.getTime();
-            let localOffset = date.getTimezoneOffset() * 60000;
-            let utc = localTime + localOffset;  
-            
-            this.timeMillis = utc + (3600000 * cityOffset);
-            this.date = new Date(this.timeMillis);
-            this.seconds = this.date.getSeconds();
-            this.minutes = this.date.getMinutes();
-            this.hours = this.date.getHours();
-            this.formattedTime = cityName + ' ' + this.date.toLocaleTimeString();
-            Time.innerHTML = this.formattedTime;
-            displayClock(this.date);
-            setInterval(this.time,1);
-        }
-    }
-};
+let timeZoneOffset = cities.value.split(",")[1];
 
-let timeDubai = new TimeData('Tallinn', 3);
-timeDubai.time();
-console.log(timeDubai.formattedTime);
+function getDate(){
+    if (!timeZoneOffset) return;
+    var date = new Date();
+    var localTime = date.getTime();
+    var localOffset = date.getTimezoneOffset() * 60000;
+    var utc = localTime + localOffset;  
+    timeMillis = utc + (3600000 * timeZoneOffset);
+    date = new Date(timeMillis);
+    Time.innerHTML = date.toLocaleTimeString();
+    displayClock(date);//returns smth like 19:24:30
+}
+setInterval(getDate,1);
+
+cities.addEventListener('click', function(){
+    timeZoneOffset = cities.value.split(",")[1];
+});
 
 function displayClock(d){
     let mhm = d.getMilliseconds() / 1000;
@@ -41,29 +34,3 @@ function displayClock(d){
 }
 
 
-/*  transform: rotate(calc(var(--start-seconds)*6deg));
-function displayTime(){
-    let clockTime = new Date();
-    let Hours = clockTime.getHours();
-    let Minutes = clockTime.getMinutes();
-    let Seconds =  clockTime.getSeconds();
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-    Time.innerHTML = + Hours + ":" + Minutes + ":" + Seconds;
-    setTimeout(displayTime,1000);
-}
-
-function  displayClock(){
-    SecondsHand.style.transform = 'rotate(calc('
-}
-
-displayTime();
-
-function checkTime(i){
-    if(i < 10){
-        i = "0" + i;
-    }
-    return i;
-}
-
-*/
